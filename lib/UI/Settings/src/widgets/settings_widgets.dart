53ae6d71f76057a0ef1b3b95728e4b201c4e9e08
Faucon <49079695+FauconSpartiate@users.noreply.github.com>
2022-11-28 11:01:31 +0100

    Attempt to fix settings color on iOS

--- lib/UI/Settings/src/widgets/settings_widgets.dart
+++ lib/UI/Settings/src/widgets/settings_widgets.dart
@@ -1098,10 +1098,7 @@ class _RadioSettingsTileState<T> extends State<RadioSettingsTile<T>> {
   Widget _buildRadioTiles(BuildContext context, T groupValue, OnChanged<T> onChanged) {
     var radioList = widget.values.entries.map<Widget>((MapEntry<T, String> entry) {
       return Material(
-        color: Theme.of(context).colorScheme.surface,
-        surfaceTintColor: Theme.of(context).colorScheme.surfaceTint,
-        elevation: 3,
-        shadowColor: Colors.transparent,
+        color: ElevationOverlay.applySurfaceTint(Theme.of(context).colorScheme.surface, Theme.of(context).colorScheme.surfaceTint, 3),
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8.0),
           child: _SettingsTile(
