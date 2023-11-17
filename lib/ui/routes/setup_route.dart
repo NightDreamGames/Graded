56e76f2d18d766d28e3a81bd4035904ed1514807
Faucon <49079695+FauconSpartiate@users.noreply.github.com>
2023-11-17 16:16:32 +0100

    Revert "Fix iOS missing back navigation in setup"
    This reverts commit 436a29e64019ed992600543e3e85252965fd1872.

--- lib/ui/routes/setup_route.dart
+++ lib/ui/routes/setup_route.dart
@@ -64,151 +64,158 @@ class _SetupPageState extends State<SetupPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: () {
-        if ((getPreference<int>("year") != -1 && (!SetupManager.hasSections() || getPreference<String>("section").isNotEmpty)) ||
-            getPreference<String>("school_system") == "other") {
-          Future.delayed(const Duration(milliseconds: 500)).then((_) {
-            if (startedAnimation || !mounted) return;
-            startedAnimation = true;
-            _animateIcon();
-          });
+    return PopScope(
+      onPopInvoked: (didPop) {
+        if (!didPop) return;
+        SetupManager.dispose();
+      },
+      child: Scaffold(
+        floatingActionButton: () {
+          if ((getPreference<int>("year") != -1 && (!SetupManager.hasSections() || getPreference<String>("section").isNotEmpty)) ||
+              getPreference<String>("school_system") == "other") {
+            Future.delayed(const Duration(milliseconds: 500)).then((_) {
+              if (startedAnimation || !mounted) return;
+              startedAnimation = true;
+              _animateIcon();
+            });
 
-          return FloatingActionButton(
-            tooltip: translations.done,
-            onPressed: () async {
-              await SetupManager.completeSetup();
+            return FloatingActionButton(
+              tooltip: translations.done,
+              onPressed: () async {
+                await SetupManager.completeSetup();
+                SetupManager.dispose();
 
-              if (!context.mounted) return;
-              replaceRoute(context);
-            },
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 800),
-              curve: Curves.elasticOut,
-              transform: Matrix4.identity()
-                ..scale(fabScale, 1.0)
-                ..translate(fabPosition),
-              child: const Icon(Icons.navigate_next),
+                if (!context.mounted) return;
+                replaceRoute(context);
+              },
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 800),
+                curve: Curves.elasticOut,
+                transform: Matrix4.identity()
+                  ..scale(fabScale, 1.0)
+                  ..translate(fabPosition),
+                child: const Icon(Icons.navigate_next),
+              ),
+            );
+          } else {
+            startedAnimation = false;
+          }
+        }(),
+        body: CustomScrollView(
+          primary: true,
+          slivers: [
+            BetterSliverAppBar.large(
+              title: AppBarTitle(
+                title: translations.setup,
+              ),
+              automaticallyImplyLeading: widget.dismissible,
             ),
-          );
-        } else {
-          startedAnimation = false;
-        }
-      }(),
-      body: CustomScrollView(
-        primary: true,
-        slivers: [
-          BetterSliverAppBar.large(
-            title: AppBarTitle(
-              title: translations.setup,
-            ),
-            automaticallyImplyLeading: widget.dismissible,
-          ),
-          SliverSafeArea(
-            top: false,
-            sliver: SliverToBoxAdapter(
-              child: SettingsContainer(
-                children: [
-                  if (!widget.dismissible)
+            SliverSafeArea(
+              top: false,
+              sliver: SliverToBoxAdapter(
+                child: SettingsContainer(
+                  children: [
+                    if (!widget.dismissible)
+                      Padding(
+                        padding: const EdgeInsets.only(left: 4, right: 4, bottom: 4),
+                        child: Card(
+                          child: ImportSettingsTile(
+                            onChanged: () {
+                              setPreference<bool>("is_first_run", false);
+                              replaceRoute(context);
+                            },
+                          ),
+                        ),
+                      ),
                     Padding(
-                      padding: const EdgeInsets.only(left: 4, right: 4, bottom: 4),
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
                       child: Card(
-                        child: ImportSettingsTile(
-                          onChanged: () {
-                            setPreference<bool>("is_first_run", false);
-                            replaceRoute(context);
-                          },
-                        ),
-                      ),
-                    ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 4),
-                    child: Card(
-                      child: RadioModalSettingsTile<String>(
-                        title: translations.school_system,
-                        icon: Icons.school,
-                        settingKey: "school_system",
-                        values: <String, String>{
-                          "lux": translations.lux_system,
-                          "other": translations.other_school_system,
-                        },
-                        onChange: (_) => rebuild(),
-                      ),
-                    ),
-                  ),
-                  if (getPreference<String>("school_system") == "lux")
-                    SettingsGroup(
-                      title: translations.lux_system,
-                      children: [
-                        RadioModalSettingsTile<String>(
-                          title: translations.system,
-                          icon: Icons.build,
-                          settingKey: "lux_system",
+                        child: RadioModalSettingsTile<String>(
+                          title: translations.school_system,
+                          icon: Icons.school,
+                          settingKey: "school_system",
                           values: <String, String>{
-                            "classic": translations.lux_system_classic,
-                            "general": translations.lux_system_general,
+                            "lux": translations.lux_system,
+                            "other": translations.other_school_system,
                           },
-                          onChange: (_) {
-                            setPreference<int>("year", DefaultValues.year);
-                            setPreference<String>("section", DefaultValues.section);
-                            setPreference<String>("variant", DefaultValues.variant);
-                            rebuild();
-                          },
+                          onChange: (_) => rebuild(),
                         ),
-                        if (getPreference<String>("lux_system").isNotEmpty)
-                          RadioModalSettingsTile<int>(
-                            title: translations.yearOne,
-                            icon: Icons.timelapse,
-                            settingKey: "year",
-                            selected: -1,
-                            values: SetupManager.getYears(),
+                      ),
+                    ),
+                    if (getPreference<String>("school_system") == "lux")
+                      SettingsGroup(
+                        title: translations.lux_system,
+                        children: [
+                          RadioModalSettingsTile<String>(
+                            title: translations.system,
+                            icon: Icons.build,
+                            settingKey: "lux_system",
+                            values: <String, String>{
+                              "classic": translations.lux_system_classic,
+                              "general": translations.lux_system_general,
+                            },
                             onChange: (_) {
-                              if (SetupManager.hasSections()) {
-                                setPreference<String>("section", DefaultValues.section);
-                              }
-                              if (SetupManager.getVariants()[getPreference<String>("variant")] == null) {
-                                setPreference<String>("variant", DefaultValues.variant);
-                              }
+                              setPreference<int>("year", DefaultValues.year);
+                              setPreference<String>("section", DefaultValues.section);
+                              setPreference<String>("variant", DefaultValues.variant);
                               rebuild();
                             },
                           ),
-                        if (SetupManager.hasSections())
-                          RadioModalSettingsTile<String>(
-                            title: translations.section,
-                            icon: Icons.fork_right,
-                            settingKey: "section",
-                            values: SetupManager.getSections(),
-                            onChange: (_) => rebuild(),
-                          ),
-                        if (SetupManager.hasVariants())
-                          RadioModalSettingsTile<String>(
-                            title: translations.variant,
-                            icon: Icons.edit,
-                            settingKey: "variant",
-                            selected: DefaultValues.variant,
-                            values: SetupManager.getVariants(),
-                            onChange: (_) => rebuild(),
-                          ),
-                        if (getPreference<int>("year") != -1 && getPreference<int>("year") != 1) const TermCountSettingsTile(),
-                      ],
-                    )
-                  else if (getPreference<String>("school_system") == "other")
-                    SettingsGroup(
-                      title: translations.other_school_system,
-                      children: getSettingsTiles(context, type: CreationType.add),
+                          if (getPreference<String>("lux_system").isNotEmpty)
+                            RadioModalSettingsTile<int>(
+                              title: translations.yearOne,
+                              icon: Icons.timelapse,
+                              settingKey: "year",
+                              selected: -1,
+                              values: SetupManager.getYears(),
+                              onChange: (_) {
+                                if (SetupManager.hasSections()) {
+                                  setPreference<String>("section", DefaultValues.section);
+                                }
+                                if (SetupManager.getVariants()[getPreference<String>("variant")] == null) {
+                                  setPreference<String>("variant", DefaultValues.variant);
+                                }
+                                rebuild();
+                              },
+                            ),
+                          if (SetupManager.hasSections())
+                            RadioModalSettingsTile<String>(
+                              title: translations.section,
+                              icon: Icons.fork_right,
+                              settingKey: "section",
+                              values: SetupManager.getSections(),
+                              onChange: (_) => rebuild(),
+                            ),
+                          if (SetupManager.hasVariants())
+                            RadioModalSettingsTile<String>(
+                              title: translations.variant,
+                              icon: Icons.edit,
+                              settingKey: "variant",
+                              selected: DefaultValues.variant,
+                              values: SetupManager.getVariants(),
+                              onChange: (_) => rebuild(),
+                            ),
+                          if (getPreference<int>("year") != -1 && getPreference<int>("year") != 1) const TermCountSettingsTile(),
+                        ],
+                      )
+                    else if (getPreference<String>("school_system") == "other")
+                      SettingsGroup(
+                        title: translations.other_school_system,
+                        children: getSettingsTiles(context, type: CreationType.add),
+                      ),
+                    SimpleSettingsTile(
+                      title: translations.note,
+                      subtitle: translations.note_description,
+                      icon: Icons.info_outline,
+                      enabled: false,
                     ),
-                  SimpleSettingsTile(
-                    title: translations.note,
-                    subtitle: translations.note_description,
-                    icon: Icons.info_outline,
-                    enabled: false,
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          const SliverPadding(padding: EdgeInsets.only(bottom: 88)),
-        ],
+            const SliverPadding(padding: EdgeInsets.only(bottom: 88)),
+          ],
+        ),
       ),
     );
   }
